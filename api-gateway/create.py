cf15919369d8c6754f3d0a5cf3bfb8fa836625ce
Ubuntu <ubuntu@ip-172-31-95-207.ec2.internal>
2021-07-07 01:16:08 +0000

    feat: create handler for put on sns

--- api-gateway/create.py
+++ api-gateway/create.py
@@ -1,16 +1,28 @@
+import boto3
 import json
-
+import os
 
 def handler(event, context):
-    usuario = event['pathParameters']['usuario'];
+    payload = json.loads(event.get('body'))
     
-    body = {
-        "message": "O usuario enviado foi: " + str(usuario)
-    }
+    # send to SNS
+    sns = boto3.client('sns')
+    snsArn=os.environ.get('snsArn', '')
 
+    res = sns.publish(
+                    TopicArn=snsArn,
+                    Subject="Order Creation",
+                    Message=json.dumps(payload),
+                    )
+
+    message={"Status":"Created","BookID": payload.get('book_id')}
+    
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(message)
     }
 
     return response
